6a103c037a5e0e04f3e7ba62258a829ef56668a5
jbarus <jakubbarus@gmail.com>
2022-12-27 12:10:41 +0100

    Wersja 0.1.0 -Zmieniłem ListView na RecyclerView w WeekView

--- app/src/main/java/com/example/organizer/WeekViewActivity.java
+++ app/src/main/java/com/example/organizer/WeekViewActivity.java
@@ -1,44 +1,47 @@
 package com.example.organizer;
 
-import static com.example.organizer.CalendarUtils.daysInMonthArray;
 import static com.example.organizer.CalendarUtils.daysInWeekArray;
 import static com.example.organizer.CalendarUtils.monthYearFromDate;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
 
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener, EventAdapter.OnEventListener
+{
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
-    private ListView eventListView;
+    private RecyclerView eventRecyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_view);
         initWidgets();
         setWeekView();
     }
 
-    private void initWidgets() {
+    private void initWidgets()
+    {
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYearText = findViewById(R.id.monthYearTV);
-        eventListView = findViewById(R.id.eventListView);
+        eventRecyclerView = findViewById(R.id.eventRecyclerView);
     }
 
-    private void setWeekView() {
+    private void setWeekView()
+    {
         monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
         ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
 
@@ -46,38 +49,53 @@ public class WeekViewActivity extends AppCompatActivity implements CalendarAdapt
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
-        setEventAadapter();
+        setEventView();
     }
 
-    public void previousWeekAction(View view) {
+
+    public void previousWeekAction(View view)
+    {
         CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
         setWeekView();
     }
 
-    public void nextWeekAction(View view) {
+    public void nextWeekAction(View view)
+    {
         CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
         setWeekView();
     }
 
-    public void onItemClick(int position, LocalDate date) {
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
         CalendarUtils.selectedDate = date;
         setWeekView();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-        setEventAadapter();
+        setEventView();
     }
 
-    private void setEventAadapter() {
+    private void setEventView()
+    {
         ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
-        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
-        eventListView.setAdapter(eventAdapter);
+        EventAdapter eventAdapter = new EventAdapter(dailyEvents, this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        eventRecyclerView.setLayoutManager(layoutManager);
+        eventRecyclerView.setAdapter(eventAdapter);
     }
 
-    public void newEventAction(View view) {
+    public void newEventAction(View view)
+    {
         startActivity(new Intent(this, EventEditActivity.class));
+    }
+
+    @Override
+    public void onEventClick() {
+        Toast.makeText(this, "Dziala", Toast.LENGTH_SHORT).show();
     }
 
     public void dailyAction(View view) {
