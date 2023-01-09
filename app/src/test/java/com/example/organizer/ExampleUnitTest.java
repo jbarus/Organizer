f2338027ace31e3b803d5f25265fa77eaaf6449b
jbarus <jakubbarus@gmail.com>
2023-01-09 19:07:46 +0100

    Dodano testy jednostkowe

--- app/src/test/java/com/example/organizer/ExampleUnitTest.java
+++ app/src/test/java/com/example/organizer/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +16,27 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getStringFromDate_isCorrect(){
+        String expected = "2023-01-08";
+        String actual = DatabasesManager.getStringFromDate(LocalDate.of(2023,1,8));
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getStringFromTime_isCorrect(){
+        String expected = "18:38:25";
+        String actual = DatabasesManager.getStringFromTime(LocalTime.of(18,38,25));
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void previousMonthAction_isCorect(){
+        Event event = new Event("Test",LocalDate.now(),LocalTime.now(),LocalTime.of(20,20,20),"Sport","Notatka");
+        String expected = "Test";
+        String actual = event.getName();
+        assertEquals(expected,actual);
     }
 }
